2b671d02ac0c9d571e9591cabf8fff41772d51b7
Giordon Stark <kratsg@gmail.com>
2020-11-13 15:48:42 -0500

    feat: CLI functionality (#2)
    * fix bump2version
    
    * huh maybe that works
    
    * add tests for coverage of the CLI
    
    * fix up logic a bit

--- src/drstorage/cli/cli.py
+++ src/drstorage/cli/cli.py
@@ -2,6 +2,7 @@ import logging
 import click
 
 from ..version import __version__
+from .. import models
 
 logging.basicConfig()
 log = logging.getLogger(__name__)
@@ -11,3 +12,21 @@ log = logging.getLogger(__name__)
 @click.version_option(version=__version__)
 def drstorage():
     pass
+
+
+@drstorage.command()
+@click.argument("bytestream", default="-", type=click.File("rb"))
+@click.option(
+    "--model",
+    "-m",
+    "model_name",
+    help="The model to use for parsing",
+    type=click.Choice(models.__all__),
+    default=models.__all__[0],
+)
+def parse(bytestream, model_name):
+    model = getattr(models, model_name)
+    line = bytestream.read(model.size)
+    while line:
+        click.echo(str(model.parse(line)).replace("\n", ""))
+        line = bytestream.read(model.size)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,26 @@
 import shlex
 import drstorage
 import time
+import subprocess
+import pytest
+
+
+@pytest.fixture(
+    scope="function",
+    params=[
+        (
+            "F1_600",
+            "abab00471200c5120901000000000000000a10025810000000000000140d0a",
+            "humidity = 7.1    temperature = 19.7",
+        )
+    ],
+    ids=["F1_600"],
+)
+def data(tmp_path, request):
+    model, input_data, stdout = request.param
+    temp = tmp_path / "data"
+    temp.write_bytes(bytearray.fromhex(input_data) * 3)
+    return temp, model, stdout
 
 
 def test_version(script_runner):
@@ -14,3 +34,24 @@ def test_version(script_runner):
     assert ret.stderr == ""
     # make sure it took less than a second
     assert elapsed < 1.0
+
+
+def test_delayed_pipe_parse(data):
+    fpath, model, stdout = data
+    command_in = f"""perl -pe "system 'sleep 1'" {fpath}"""
+    command = f"drstorage parse --model {model}"
+    ps = subprocess.Popen(shlex.split(command_in), stdout=subprocess.PIPE)
+    output = subprocess.check_output(shlex.split(command), stdin=ps.stdout)
+    assert ps.stdout.read() == b"", "All of the piped input was not read in"
+    assert len(output) > 100, "More output was expected"
+    assert stdout in output.decode("utf-8")
+
+
+def test_parse(script_runner, data):
+    fpath, model, stdout = data
+    command = f"drstorage parse --model {model} {fpath}"
+    ret = script_runner.run(*shlex.split(command))
+    assert ret.success
+    assert len(ret.stdout) > 100
+    assert stdout in ret.stdout
+    assert ret.stderr == ""
